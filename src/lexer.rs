3e4da58c57b358942bf19e29a175d1c3cf67e1ed
Brandon Barker <brandon.barker@mechanicalrock.io>
2025-01-29 18:11:49 +0800

    lexer

---
+++ src/lexer.rs
@@ -0,0 +1,154 @@
+use logos::Logos;
+
+#[derive(Debug, Logos, PartialEq)]
+#[logos(skip r"[ \t\n\r]+")]
+pub enum Token<'source> {
+    #[regex(r"[0-9]+", |lex| lex.slice().parse::<u32>().ok(), priority = 3)]
+    Number(u32),
+
+    #[token("x", priority = 4)]
+    Times,
+
+    #[token("{")]
+    BraceOpen,
+
+    #[token("}")]
+    BraceClose,
+
+    #[token("m", priority = 4)]
+    Meters,
+
+    #[token("km", priority = 4)]
+    Kilometers,
+
+    #[regex(r"[a-zA-Z][a-zA-Z.-]*", |lex| lex.slice(), priority = 2)]
+    Word(&'source str),
+
+    #[token("(")]
+    ParenOpen,
+
+    #[token(")")]
+    ParenClose,
+
+    #[token(",")]
+    Comma,
+
+    #[token("@")]
+    At,
+
+    #[regex(r"[0-9]+s", |lex| lex.slice().trim_end_matches('s').parse::<u32>().ok())]
+    Seconds(u32),
+
+    #[regex(r"[0-9]+:[0-9]+s?", |lex| lex.slice())]
+    Time(&'source str),
+
+    #[regex(r"#[^\n]*", logos::skip)]
+    #[regex(r"//[^\n]*", logos::skip)]
+    #[regex(r"/\*([^*]|\*[^/])*\*/", logos::skip)]
+    Comment,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use logos::Logos;
+
+    #[test]
+    fn test_basic_tokens() {
+        let mut lex = Token::lexer("100");
+
+        assert_eq!(lex.next(), Some(Ok(Token::Number(100))));
+        assert_eq!(lex.span(), 0..3);
+        assert_eq!(lex.slice(), "100");
+
+        assert_eq!(lex.next(), None);
+    }
+
+    #[test]
+    fn test_complex_statements() {
+        let mut lex = Token::lexer("100m freestyle @1:30");
+
+        assert_eq!(lex.next(), Some(Ok(Token::Number(100))));
+        assert_eq!(lex.span(), 0..3);
+        assert_eq!(lex.slice(), "100");
+
+        assert_eq!(lex.next(), Some(Ok(Token::Meters)));
+        assert_eq!(lex.span(), 3..4);
+        assert_eq!(lex.slice(), "m");
+
+        assert_eq!(lex.next(), Some(Ok(Token::Word("freestyle"))));
+        assert_eq!(lex.span(), 5..14);
+        assert_eq!(lex.slice(), "freestyle");
+
+        assert_eq!(lex.next(), Some(Ok(Token::At)));
+        assert_eq!(lex.span(), 15..16);
+        assert_eq!(lex.slice(), "@");
+
+        assert_eq!(lex.next(), Some(Ok(Token::Time("1:30"))));
+        assert_eq!(lex.span(), 16..20);
+        assert_eq!(lex.slice(), "1:30");
+
+        assert_eq!(lex.next(), None);
+    }
+
+    #[test]
+    fn test_modifiers() {
+        let mut lex = Token::lexer("butterfly(drill, kick)");
+
+        assert_eq!(lex.next(), Some(Ok(Token::Word("butterfly"))));
+        assert_eq!(lex.span(), 0..9);
+        assert_eq!(lex.slice(), "butterfly");
+
+        assert_eq!(lex.next(), Some(Ok(Token::ParenOpen)));
+        assert_eq!(lex.span(), 9..10);
+        assert_eq!(lex.slice(), "(");
+
+        assert_eq!(lex.next(), Some(Ok(Token::Word("drill"))));
+        assert_eq!(lex.span(), 10..15);
+        assert_eq!(lex.slice(), "drill");
+
+        assert_eq!(lex.next(), Some(Ok(Token::Comma)));
+        assert_eq!(lex.span(), 15..16);
+        assert_eq!(lex.slice(), ",");
+
+        assert_eq!(lex.next(), Some(Ok(Token::Word("kick"))));
+        assert_eq!(lex.span(), 17..21);
+        assert_eq!(lex.slice(), "kick");
+
+        assert_eq!(lex.next(), Some(Ok(Token::ParenClose)));
+        assert_eq!(lex.span(), 21..22);
+        assert_eq!(lex.slice(), ")");
+
+        assert_eq!(lex.next(), None);
+    }
+
+    #[test]
+    fn test_intervals() {
+        let mut lex = Token::lexer("@30s");
+
+        assert_eq!(lex.next(), Some(Ok(Token::At)));
+        assert_eq!(lex.span(), 0..1);
+        assert_eq!(lex.slice(), "@");
+
+        assert_eq!(lex.next(), Some(Ok(Token::Seconds(30))));
+        assert_eq!(lex.span(), 1..4);
+        assert_eq!(lex.slice(), "30s");
+
+        assert_eq!(lex.next(), None);
+    }
+
+    #[test]
+    fn test_comments() {
+        let mut lex = Token::lexer("100m # comment");
+
+        assert_eq!(lex.next(), Some(Ok(Token::Number(100))));
+        assert_eq!(lex.span(), 0..3);
+        assert_eq!(lex.slice(), "100");
+
+        assert_eq!(lex.next(), Some(Ok(Token::Meters)));
+        assert_eq!(lex.span(), 3..4);
+        assert_eq!(lex.slice(), "m");
+
+        assert_eq!(lex.next(), None);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,149 +1,22 @@
-use std::{collections::HashMap, time::Duration};
+use clap::Parser;
 
-use chumsky::prelude::*;
-use serde::Serialize;
+use logos::Logos;
+use swim_parser::lexer::Token;
 
-#[derive(Debug, Serialize)]
-enum Set {
-    Repetition {
-        number: u32,
-        set: Box<Set>,
-    },
-    Statement {
-        distance: u32,
-        stroke: String,
-        interval: u32,
-    },
-    Block(Vec<Set>),
-}
-
-impl Set {
-    fn total_distance(&self) -> u32 {
-        match self {
-            Set::Repetition { number, set } => number * set.total_distance(),
-            Set::Statement { distance, .. } => *distance,
-            Set::Block(blocks) => blocks.iter().map(|block| block.total_distance()).sum(),
-        }
-    }
-
-    fn total_duration(&self) -> u32 {
-        match self {
-            Set::Repetition { number, set } => set.total_duration().checked_mul(*number).unwrap(),
-            Set::Statement { interval, .. } => *interval,
-            Set::Block(blocks) => blocks.iter().map(|block| block.total_duration()).sum(),
-        }
-    }
-
-    fn distribution(&self) -> HashMap<&str, u32> {
-        let mut dist = HashMap::new();
-        distribution_helper(self, &mut dist);
-        dist
-    }
-}
-
-fn distribution_helper<'a>(set: &'a Set, strokes: &mut HashMap<&'a str, u32>) {
-    match set {
-        Set::Repetition { number, set } => {
-            for _ in 0..*number {
-                distribution_helper(set, strokes);
-            }
-        }
-        Set::Statement {
-            distance, stroke, ..
-        } => {
-            let count = strokes.entry(stroke).or_insert(0);
-            *count += distance;
-        }
-        Set::Block(blocks) => {
-            for block in blocks {
-                distribution_helper(block, strokes);
-            }
-        }
-    }
-}
-
-#[allow(dead_code)]
-#[derive(Debug)]
-enum Stroke {
-    Free,
-    Back,
-    Breast,
-    Fly,
-    IM,
-    Kick,
-    Drill,
-    Choice,
-}
-
-#[allow(clippy::let_and_return)]
-fn parser() -> impl Parser<char, Set, Error = Simple<char>> {
-    let distance = text::int(10).then_ignore(just('m'));
-
-    let stroke = filter(|&x| char::is_alphabetic(x) || x.is_ascii_punctuation())
-        .repeated()
-        .at_least(1)
-        .collect::<String>();
-
-    let interval = just("@").padded().ignore_then(
-        text::int(10)
-            .then_ignore(just('s'))
-            .map(|x| x.parse::<u32>().unwrap())
-            .or(text::int(10)
-                .then_ignore(just(':'))
-                .then(text::digits(10))
-                .map(|(x, y)| {
-                    let minutes: u32 = x.parse().unwrap();
-                    let seconds: u32 = y.parse().unwrap();
-                    minutes * 60 + seconds
-                })),
-    );
-
-    let statement = distance
-        .then(stroke.padded())
-        .then(interval.padded())
-        .map(|((distance, stroke), interval)| Set::Statement {
-            stroke,
-            distance: distance.parse().unwrap(),
-            interval,
-        })
-        .padded();
-
-    let repetition = recursive(|rep| {
-        let block = rep
-            .or(statement)
-            .repeated()
-            .map(Set::Block)
-            .delimited_by(just("{"), just("}"))
-            .padded();
-
-        let repetition = text::int(10)
-            .then_ignore(just('x').padded())
-            .then(block.or(statement))
-            .map(|(reps, set)| Set::Repetition {
-                number: reps.parse().unwrap(),
-                set: Box::new(set),
-            });
-
-        repetition
-    });
-
-    repetition.or(statement).repeated().map(Set::Block)
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    set: String,
 }
 
 fn main() {
-    let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
+    let args = Args::parse();
 
-    match parser().parse(src) {
-        Ok(set) => {
-            println!("{:#?}", set);
-            println!("TOTAL DISTANCE = {}m", set.total_distance());
-            println!("DISTRIBUTION = {:#?}", set.distribution());
-            println!("DURATION = {:#?}", set.total_duration());
-            let serialized = serde_json::to_string(&set).unwrap();
-            println!("serialized = {}", serialized);
-        }
-        Err(parse_errs) => parse_errs
-            .into_iter()
-            .for_each(|e| println!("Parse error: {}", e)),
+    let set_file = std::fs::read_to_string(args.set).unwrap();
+
+    let mut lexer = Token::lexer(&set_file);
+
+    while let Some(token) = lexer.next() {
+        println!("{:?}: {:?}", token, lexer.slice());
     }
 }
